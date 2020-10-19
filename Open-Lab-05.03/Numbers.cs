976bf007077cbd297f218eeee0d19eee6f2183de
Jaro32768 <71781871+Jaro32768@users.noreply.github.com>
2020-10-19 17:55:56 +0200

    vypracovanie

--- Open-Lab-05.03/Numbers.cs
+++ Open-Lab-05.03/Numbers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace Open_Lab_05._03
 {
@@ -6,7 +7,91 @@ namespace Open_Lab_05._03
     {
         public string FormatNum(int num)
         {
-            throw new NotImplementedException();
+            string n = num.ToString();
+            int a = 0;
+            string ret = "";
+            string z = "";
+            if (n.Length < 4)
+            {
+                ret = n;
+            }
+            else if (n.Length < 7)
+            {
+                switch (n.Length % 3)
+                {
+                    case 1:
+                        {
+                            ret = "" + n[0] + ",";
+                            a = 1;
+                            break;
+                        }
+                    case 2:
+                        {
+                            ret = "" + n[0] + n[1] + ",";
+                            a = 2;
+                            break;
+                        }
+                    default:
+                        {
+                            ret = "";
+                            a = 0;
+                            break;
+                        }
+                }
+                for (int i = a; i < n.Length; i++)
+                {
+                    z = z + n[i];
+                }
+                for (int i = 0; i < z.Length - 1; i = i + 3)
+                {
+                    ret = ret + z[i] + z[i + 1] + z[i + 2] + ",";
+                }
+                string q = ret;
+                ret = "";
+                for (int i = 0; i < q.Length - 1; i++)
+                {
+                    ret = ret + q[i];
+                }
+            }
+            else
+            {
+                switch (n.Length % 3)
+                {
+                    case 1:
+                        {
+                            ret = "" + n[0] + Convert.ToChar(160);
+                            a = 1;
+                            break;
+                        }
+                    case 2:
+                        {
+                            ret = "" + n[0] + n[1] + Convert.ToChar(160);
+                            a = 2;
+                            break;
+                        }
+                    default:
+                        {
+                            ret = "";
+                            a = 0;
+                            break;
+                        }
+                }
+                for (int i = a; i < n.Length; i++)
+                {
+                    z = z + n[i];
+                }
+                for (int i = 0; i < z.Length - 1; i = i + 3)
+                {
+                    ret = ret + z[i] + z[i + 1] + z[i + 2] + Convert.ToChar(160);
+                }
+                string q = ret;
+                ret = "";
+                for (int i = 0; i < q.Length - 1; i++)
+                {
+                    ret = ret + q[i];
+                }
+            }
+            return ret;
         }
     }
 }
